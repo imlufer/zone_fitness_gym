1c2db6b46e4920a619db942e96d57292319e3301
Fernando Yarleque <fernando.yarlequeg@gmail.com>
2024-04-27 15:17:04 -0500

    feat: efecto hidden

--- js/script.js
+++ js/script.js
@@ -9,3 +9,65 @@ function toggleMenu(){
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
 }
+//ANIMACION 
+// Obtener todos los elementos que deseas animar
+const clases = document.querySelectorAll('.sec-clases');
+const couches = document.querySelectorAll('.sec-couches');
+const contacto = document.querySelectorAll('.sec-contacto');
+
+
+// Función para verificar la posición de los elementos y mostrarlos si están en el viewport
+function showElementsOnScroll() {
+    clases.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Si el elemento está más abajo que la mitad de la ventana gráfica, mostrarlo
+        if (elementTop < windowHeight / 2) {
+            element.classList.add('visible');
+            element.classList.remove('hidden');
+        } else {
+            element.classList.add('hidden');
+            element.classList.remove('visible');
+        }
+    });
+    couches.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Si el elemento está más abajo que la mitad de la ventana gráfica, mostrarlo
+        if (elementTop < windowHeight / 2) {
+            element.classList.add('visible');
+            element.classList.remove('hidden');
+        } else {
+            element.classList.add('hidden');
+            element.classList.remove('visible');
+        }
+    });
+    contacto.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        // Si el elemento está más abajo que la mitad de la ventana gráfica, mostrarlo
+        if (elementTop < windowHeight / 2) {
+            element.classList.add('visible');
+            element.classList.remove('hidden');
+        } else {
+            element.classList.add('hidden');
+            element.classList.remove('visible');
+        }
+    });
+   
+}
+
+
+// Función para manejar la animación al hacer scroll
+function handleScroll() {
+    window.requestAnimationFrame(showElementsOnScroll);
+}
+
+// Agregar un listener para el evento scroll
+window.addEventListener('scroll', handleScroll);
+
+// Llamar a la función una vez al cargar la página para verificar el estado inicial
+showElementsOnScroll();
